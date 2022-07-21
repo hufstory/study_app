087591a39f1fab8cc27505963442d629e1c9cd23
SEUNGJUN JEONG <jsj9620@naver.com>
2022-07-22 00:20:00 +0900

    changed timetable
    added timetable widget (used package)

--- login1/lib/mainpage.dart
+++ login1/lib/mainpage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_timetable_view/flutter_timetable_view.dart';
 
 void main() => runApp(const MyApp());
 
@@ -13,10 +14,10 @@ class MyApp extends StatelessWidget {
     return Container(
       decoration: const BoxDecoration(
           gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [Color(0xffe5cdde), Color(0xff9b7fc1)],
-          )),
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [Color(0xffe5cdde), Color(0xff9b7fc1)],
+      )),
       child: const MaterialApp(
         title: 'mainpage',
         debugShowCheckedModeBanner: false,
@@ -38,21 +39,21 @@ class MainPage extends StatelessWidget {
         child: AppBar(
           title: const Text(' '),
           centerTitle: true,
-          backgroundColor: Colors.transparent, // 앱바 투명
+          backgroundColor: Colors.transparent,
+          // 앱바 투명
           elevation: 0.0,
           actions: [
             Builder(
               // Drawer 아이콘 색 지정 위해 Builder 위젯 사용
-              builder: (context) =>
-                  IconButton(
-                    icon: const Icon(
-                      Icons.menu,
-                      color: Colors.black,
-                    ),
-                    onPressed: () {
-                      Scaffold.of(context).openEndDrawer(); // Drawer 열음
-                    },
-                  ),
+              builder: (context) => IconButton(
+                icon: const Icon(
+                  Icons.menu,
+                  color: Colors.black,
+                ),
+                onPressed: () {
+                  Scaffold.of(context).openEndDrawer(); // Drawer 열음
+                },
+              ),
             )
           ],
         ),
@@ -153,48 +154,48 @@ class MainPage extends StatelessWidget {
                   ),
                 ),
               ),
-              Stack( // 타이머 부분
+              Stack(// 타이머 부분
                   children: [
-                    Container(
-                      padding: const EdgeInsets.only(bottom: 15.0, right: 25.0),
-                      alignment: Alignment.bottomRight,
-                      width: 180,
-                      height: 180,
-                      decoration: const BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.all(Radius.circular(30)),
-                      ),
-                      child: RichText(
-                        text: const TextSpan(
-                            text: '3',
-                            style: TextStyle(
-                                color: Color(0xff645E5E),
-                                fontSize: 35,
-                                fontWeight: FontWeight.bold),
-                            children: [
-                              TextSpan(
-                                  text: 'H',
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 35,
-                                      fontWeight: FontWeight.bold)),
-                              TextSpan(
-                                  text: ' 47',
-                                  style: TextStyle(
-                                      color: Color(0xff645E5E),
-                                      fontSize: 35,
-                                      fontWeight: FontWeight.bold)),
-                              TextSpan(
-                                  text: 'M',
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 35,
-                                      fontWeight: FontWeight.bold))
-                            ]),
-                      ),
-                    ),
-                    Image.asset('assets/flower.png', width: 120, height: 120)
-                  ]),
+                Container(
+                  padding: const EdgeInsets.only(bottom: 15.0, right: 25.0),
+                  alignment: Alignment.bottomRight,
+                  width: 180,
+                  height: 180,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                  ),
+                  child: RichText(
+                    text: const TextSpan(
+                        text: '3',
+                        style: TextStyle(
+                            color: Color(0xff645E5E),
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold),
+                        children: [
+                          TextSpan(
+                              text: 'H',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.bold)),
+                          TextSpan(
+                              text: ' 47',
+                              style: TextStyle(
+                                  color: Color(0xff645E5E),
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.bold)),
+                          TextSpan(
+                              text: 'M',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.bold))
+                        ]),
+                  ),
+                ),
+                Image.asset('assets/flower.png', width: 120, height: 120)
+              ]),
             ],
           ),
           Container(
@@ -220,150 +221,113 @@ class MainPage extends StatelessWidget {
               ],
             ),
           ),
-          Stack(alignment: Alignment.center, children: [
-            Container(
-              // 시간표
-              width: 377.1,
-              height: 330,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20.0)),
-            ),
-            Container(
-              // 시간표 안쪽
-              width: 365,
-              height: 320,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(
-                      color: Colors.grey,
-                      width: 0.6
-                  ),
-                  borderRadius: BorderRadius.circular(20.0)),
-              child: Column(
-                children: const [ // 시간표 가로줄
-                  SizedBox(height: 15.0),
-                  Divider(thickness: 0.6, color: Colors.grey),
-                  SizedBox(height: 25.5),
-                  Divider(thickness: 0.6, color: Colors.grey),
-                  SizedBox(height: 25.5),
-                  Divider(thickness: 0.6, color: Colors.grey),
-                  SizedBox(height: 25.5),
-                  Divider(thickness: 0.6, color: Colors.grey),
-                  SizedBox(height: 25.5),
-                  Divider(thickness: 0.6, color: Colors.grey),
-                  SizedBox(height: 25.5),
-                  Divider(thickness: 0.6, color: Colors.grey),
-                  SizedBox(height: 25.5),
-                  Divider(thickness: 0.6, color: Colors.grey),
-                ],
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                // 시간표
+                width: 377.1,
+                height: 330,
+                padding: EdgeInsets.all(5.7),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20.0)),
+                child: TimeTable(),
               ),
-            ),
-            Container(
-              // 시간표 세로선
-              width: 365,
-              height: 320,
-              child: Row(children: const [
-                SizedBox(width: 15.0),
-                VerticalDivider(color: Colors.grey, thickness: 0.6),
-                SizedBox(width: 52),
-                VerticalDivider(color: Colors.grey, thickness: 0.6),
-                SizedBox(width: 52),
-                VerticalDivider(color: Colors.grey, thickness: 0.6),
-                SizedBox(width: 52),
-                VerticalDivider(color: Colors.grey, thickness: 0.6),
-                SizedBox(width: 52),
-                VerticalDivider(color: Colors.grey, thickness: 0.6),
-              ]),
-            ),
-            Positioned(
-                left: 55,
-                top: 6.5,
-                child: Container(
-                  child: Text('월', style: TextStyle(color: Colors.grey)),
-                )
-            ),
-            Positioned(
-                left: 125,
-                top: 6.5,
-                child: Container(
-                  child: Text('화', style: TextStyle(color: Colors.grey)),
-                )
-            ),
-            Positioned(
-                left: 192,
-                top: 6.5,
-                child: Container(
-                  child: Text('수', style: TextStyle(color: Colors.grey)),
-                )
-            ),
-            Positioned(
-                left: 260,
-                top: 6.5,
-                child: Container(
-                  child: Text('목', style: TextStyle(color: Colors.grey)),
-                )
-            ),
-            Positioned(
-                left: 330,
-                top: 6.5,
-                child: Container(
-                  child: Text('금', style: TextStyle(color: Colors.grey)),
-                )
-            ),
-            Positioned(
-              left: 20,
-              top: 30,
-              child: Container(
-                child: Text('9', style: TextStyle(color: Colors.grey),),
-              ),
-            ),
-            Positioned(
-              left: 12,
-              top: 70,
-              child: Container(
-                child: Text('10', style: TextStyle(color: Colors.grey),),
-              ),
-            ),
-            Positioned(
-              left: 12,
-              top: 112,
-              child: Container(
-                child: Text('11', style: TextStyle(color: Colors.grey),),
-              ),
-            ),
-            Positioned(
-              left: 12,
-              top: 153,
-              child: Container(
-                child: Text('12', style: TextStyle(color: Colors.grey),),
-              ),
-            ),
-            Positioned(
-              left: 20,
-              top: 194,
-              child: Container(
-                child: Text('1', style: TextStyle(color: Colors.grey),),
-              ),
-            ),
-            Positioned(
-              left: 19,
-              top: 235,
-              child: Container(
-                child: Text('2', style: TextStyle(color: Colors.grey),),
-              ),
-            ),
-            Positioned(
-              left: 19,
-              top: 277,
-              child: Container(
-                child: Text('3', style: TextStyle(color: Colors.grey),),
-              ),
-            ),
-          ],
+            ],
           ),
         ],
       ),
     );
+  }
+}
+
+// 시간표 위젯, 사용 패키지: flutter_timetable_view
+class TimeTable extends StatefulWidget {
+  const TimeTable({Key? key}) : super(key: key);
+
+  @override
+  State<TimeTable> createState() => _TimeTableState();
+}
+
+class _TimeTableState extends State<TimeTable> {
+  @override
+  Widget build(BuildContext context) {
+    return TimetableView(
+      laneEventsList: _buildLaneEvents(),
+      timetableStyle: const TimetableStyle(
+          startHour: 9,
+          endHour: 19,
+          laneWidth: 62,
+          laneHeight: 20,
+          decorationLineBorderColor: Colors.grey,
+          timeItemTextColor: Colors.grey,
+          timeItemHeight: 45,
+          timeItemWidth: 50),
+    );
+  }
+
+  List<LaneEvents> _buildLaneEvents() {
+    return [
+      LaneEvents(
+          lane: Lane(
+              name: '월', width: 60, textStyle: TextStyle(color: Colors.grey)),
+          events: [
+            TableEvent(
+                title: '선형대수',
+                start: TableEventTime(hour: 9, minute: 30),
+                end: TableEventTime(hour: 11, minute: 20),
+                decoration: BoxDecoration(color: Colors.orange),
+                textStyle: TextStyle(fontSize: 12),
+                padding: EdgeInsets.all(3.0)),
+          ]),
+      LaneEvents(
+          lane: Lane(
+              name: '화', width: 60, textStyle: TextStyle(color: Colors.grey)),
+          events: [
+            TableEvent(
+                title: '자료구조',
+                start: TableEventTime(hour: 11, minute: 30),
+                end: TableEventTime(hour: 13, minute: 20),
+                decoration: BoxDecoration(color: Colors.red),
+                textStyle: TextStyle(fontSize: 12),
+                padding: EdgeInsets.all(3.0))
+          ]),
+      LaneEvents(
+          lane: Lane(
+              name: '수', width: 60, textStyle: TextStyle(color: Colors.grey)),
+          events: [
+            TableEvent(
+                title: '수리통계학',
+                start: TableEventTime(hour: 13, minute: 30),
+                end: TableEventTime(hour: 15, minute: 20),
+                decoration: BoxDecoration(color: Colors.green),
+                textStyle: TextStyle(fontSize: 12),
+                padding: EdgeInsets.all(3.0)),
+            TableEvent(
+                title: '컴퓨팅사고',
+                start: TableEventTime(hour: 15, minute: 30),
+                end: TableEventTime(hour: 17, minute: 20),
+                decoration: BoxDecoration(color: Colors.blue),
+                textStyle: TextStyle(fontSize: 12),
+                padding: EdgeInsets.all(3.0)),
+          ]),
+      LaneEvents(
+          lane: Lane(
+              name: '목', width: 60, textStyle: TextStyle(color: Colors.grey)),
+          events: []),
+      LaneEvents(
+          lane: Lane(
+              name: '금', width: 60, textStyle: TextStyle(color: Colors.grey)),
+          events: [
+            TableEvent(
+                title: '알고리즘',
+                start: TableEventTime(hour: 11, minute: 30),
+                end: TableEventTime(hour: 13, minute: 20),
+                decoration: BoxDecoration(color: Colors.purple),
+                textStyle: TextStyle(fontSize: 12),
+                padding: EdgeInsets.all(3.0)),
+          ]),
+    ];
   }
 }
