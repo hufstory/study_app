8bb633241f734e7920bf413de9f9a953f0ac0552
SEUNGJUN JEONG <jsj9620@naver.com>
2022-07-31 15:36:33 +0900

    (Mainpage) 시간표와 스터디 목록 연동

--- login1/lib/mainpage.dart
+++ login1/lib/mainpage.dart
@@ -10,13 +10,15 @@ FirebaseFirestore db = FirebaseFirestore.instance;
 Set<String> subjectList = {};
 List<String> dayList = [];
 List docList = [];
+Map<String, String> sortList = {};
 
 var user = FirebaseAuth.instance.currentUser;
 var uid = user?.uid;
 
-readData() async {
+Future readData() async {
   // 과목명 불러오기
-  db.collection(uid!).snapshots().listen((QuerySnapshot qs) {
+  db.collection(uid!).snapshots().listen(
+    (QuerySnapshot qs) {
       qs.docs.forEach((doc) => subjectList.add(doc["subject"]));
       qs.docs.forEach((doc) => dayList.add(doc["day"]));
     },
@@ -24,15 +26,13 @@ readData() async {
   db.collection(uid!).snapshots().listen((QuerySnapshot qs) {
     qs.docs.forEach((doc) => docList.add(doc.data()));
     print(docList[0]);
+    print(docList[0]["day"]);
   });
-  print(subjectList);
 }
-
 
 
 class mainPage extends StatefulWidget {
   const mainPage({Key? key}) : super(key: key);
-
 
   @override
   State<mainPage> createState() => _mainPageState();
@@ -40,17 +40,18 @@ class mainPage extends StatefulWidget {
 
 class _mainPageState extends State<mainPage> {
   @override
-  void initState() {
+  initState() {
     readData();
     super.initState();
+    print(docList);
   }
+
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations(
         [DeviceOrientation.portraitUp]); // 가로방향 못돌리게
     return Container(
       decoration: const BoxDecoration(
-          image: DecorationImage(image: AssetImage('assets/background.png'))
-      ),
+          image: DecorationImage(image: AssetImage('assets/background.png'))),
       child: MaterialApp(
         title: 'mainpage',
         debugShowCheckedModeBanner: false,
@@ -152,100 +153,100 @@ class _MainPageState extends State<MainPage> {
         ),
       ),
       body: StreamBuilder<QuerySnapshot>(
-        stream: db.collection(uid!).snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return CircularProgressIndicator();
-          }
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    // 스터디 목록 부분
-                    width: 180,
-                    height: 180,
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                      image: DecorationImage(
-                        image: AssetImage('assets/grass.png'),
+          stream: db.collection(uid!).snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            }
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      // 스터디 목록 부분
+                      width: 180,
+                      height: 180,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                        image: DecorationImage(
+                          image: AssetImage('assets/grass.png'),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: subjectList.map((e) => Text(e)).toList()),
                       ),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: subjectList.map((e) => Text(e)).toList()),
-                    ),
-                  ),
-                  Stack(// 타이머 부분
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.only(bottom: 15.0, right: 25.0),
-                          alignment: Alignment.bottomRight,
-                          width: 180,
-                          height: 180,
-                          decoration: const BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.all(Radius.circular(30)),
-                          ),
-                          child: RichText(
-                            text: const TextSpan(
-                                text: '3',
-                                style: TextStyle(
-                                    color: Color(0xff645E5E),
-                                    fontSize: 35,
-                                    fontWeight: FontWeight.bold),
-                                children: [
-                                  TextSpan(
-                                      text: 'H',
-                                      style: TextStyle(
-                                          color: Colors.black,
-                                          fontSize: 35,
-                                          fontWeight: FontWeight.bold)),
-                                  TextSpan(
-                                      text: ' 47',
-                                      style: TextStyle(
-                                          color: Color(0xff645E5E),
-                                          fontSize: 35,
-                                          fontWeight: FontWeight.bold)),
-                                  TextSpan(
-                                      text: 'M',
-                                      style: TextStyle(
-                                          color: Colors.black,
-                                          fontSize: 35,
-                                          fontWeight: FontWeight.bold))
-                                ]),
-                          ),
+                    Stack(// 타이머 부분
+                        children: [
+                      Container(
+                        padding:
+                            const EdgeInsets.only(bottom: 15.0, right: 25.0),
+                        alignment: Alignment.bottomRight,
+                        width: 180,
+                        height: 180,
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
                         ),
-                        Image.asset('assets/flower.png', width: 120, height: 120)
-                      ]),
-                ],
-              ),
-              Stack(
-                alignment: Alignment.center,
-                children: [
-                  Container(
-                    // 시간표
-                    width: 377.1,
-                    height: 330,
-                    padding: EdgeInsets.all(5.7),
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(20.0)),
-                    child: TimeTable(
-                      subjectList1: [...subjectList],
+                        child: RichText(
+                          text: const TextSpan(
+                              text: '3',
+                              style: TextStyle(
+                                  color: Color(0xff645E5E),
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.bold),
+                              children: [
+                                TextSpan(
+                                    text: 'H',
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 35,
+                                        fontWeight: FontWeight.bold)),
+                                TextSpan(
+                                    text: ' 47',
+                                    style: TextStyle(
+                                        color: Color(0xff645E5E),
+                                        fontSize: 35,
+                                        fontWeight: FontWeight.bold)),
+                                TextSpan(
+                                    text: 'M',
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 35,
+                                        fontWeight: FontWeight.bold))
+                              ]),
+                        ),
+                      ),
+                      Image.asset('assets/flower.png', width: 120, height: 120)
+                    ]),
+                  ],
+                ),
+                Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      // 시간표
+                      width: 377.1,
+                      height: 330,
+                      padding: EdgeInsets.all(5.7),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20.0)),
+                      child: TimeTable(
+                        subjectList1: [...subjectList],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
-          );
-        }
-      ),
+                  ],
+                ),
+              ],
+            );
+          }),
     );
   }
 }
@@ -255,7 +256,6 @@ class TimeTable extends StatefulWidget {
 
   final List subjectList1; // 스터디 과목 리스트
 
-  final List<String> dayList = ['월', '화', '수', '목', '금'];
 
   @override
   State<TimeTable> createState() => _TimeTableState();
@@ -264,66 +264,155 @@ class TimeTable extends StatefulWidget {
 class _TimeTableState extends State<TimeTable> {
   @override
   Widget build(BuildContext context) {
-    return TimetableView(
-      laneEventsList: _buildLaneEvents(),
-      timetableStyle: const TimetableStyle(
-          startHour: 9,
-          endHour: 19,
-          laneWidth: 62,
-          laneHeight: 20,
-          decorationLineBorderColor: Colors.grey,
-          timeItemTextColor: Colors.grey,
-          timeItemHeight: 45,
-          timeItemWidth: 50),
+    return StreamBuilder<QuerySnapshot>(
+      stream: db.collection(uid!).snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator(),);
+        }
+        return TimetableView(
+          laneEventsList: _buildLaneEvents(),
+          timetableStyle: const TimetableStyle(
+              startHour: 9,
+              endHour: 19,
+              laneWidth: 62,
+              laneHeight: 20,
+              decorationLineBorderColor: Colors.grey,
+              timeItemTextColor: Colors.grey,
+              timeItemHeight: 45,
+              timeItemWidth: 50),
+        );
+      }
     );
   }
 
-  List<TableEvent> _buildTableEvent() {
-    return [
-      TableEvent(
-          title: widget.subjectList1[0].toString(),
-          start: TableEventTime(
-              hour: docList[0]["startHour"], minute: docList[0]["startMin"]),
-          end: TableEventTime(
-              hour: docList[0]["endHour"], minute: docList[0]["endMin"]),
-          decoration: BoxDecoration(color: Colors.blue),
-          textStyle: TextStyle(fontSize: 12),
-          padding: EdgeInsets.all(3.0))
-    ];
+  _buildTableEvent1() {
+    for (int i = 0; i < docList.length; i++) {
+      if (docList[i]["day"] == "월") {
+        return [
+          TableEvent(
+              title: docList[i]["subject"].toString(),
+              start: TableEventTime(
+                  hour: docList[i]["startHour"],
+                  minute: docList[i]["startMin"]),
+              end: TableEventTime(
+                  hour: docList[i]["endHour"], minute: docList[i]["endMin"]),
+              decoration: BoxDecoration(color: Colors.blue),
+              textStyle: TextStyle(fontSize: 12),
+              padding: EdgeInsets.all(3.0))
+        ];
+      }
+    }
+  }
+
+  _buildTableEvent2() {
+    for (int i = 0; i < docList.length; i++) {
+      if (docList[i]["day"] == "화") {
+        return [
+          TableEvent(
+              title: docList[i]["subject"].toString(),
+              start: TableEventTime(
+                  hour: docList[i]["startHour"],
+                  minute: docList[i]["startMin"]),
+              end: TableEventTime(
+                  hour: docList[i]["endHour"], minute: docList[i]["endMin"]),
+              decoration: BoxDecoration(color: Colors.blue),
+              textStyle: TextStyle(fontSize: 12),
+              padding: EdgeInsets.all(3.0))
+        ];
+      }
+    }
+  }
+
+  _buildTableEvent3() {
+    for (int i = 0; i < docList.length; i++) {
+      if (docList[i]["day"] == "수") {
+        return [
+          TableEvent(
+              title: docList[i]["subject"].toString(),
+              start: TableEventTime(
+                  hour: docList[i]["startHour"],
+                  minute: docList[i]["startMin"]),
+              end: TableEventTime(
+                  hour: docList[i]["endHour"], minute: docList[i]["endMin"]),
+              decoration: BoxDecoration(color: Colors.blue),
+              textStyle: TextStyle(fontSize: 12),
+              padding: EdgeInsets.all(3.0))
+        ];
+      }
+    }
+  }
+
+  _buildTableEvent4() {
+    for (int i = 0; i < docList.length; i++) {
+      if (docList[i]["day"] == "목") {
+        return [
+          TableEvent(
+              title: docList[i]["subject"].toString(),
+              start: TableEventTime(
+                  hour: docList[i]["startHour"],
+                  minute: docList[i]["startMin"]),
+              end: TableEventTime(
+                  hour: docList[i]["endHour"], minute: docList[i]["endMin"]),
+              decoration: BoxDecoration(color: Colors.blue),
+              textStyle: TextStyle(fontSize: 12),
+              padding: EdgeInsets.all(3.0))
+        ];
+      }
+    }
+  }
+
+  _buildTableEvent5() {
+    for (int i = 0; i < docList.length; i++) {
+      if (docList[i]["day"] == "금") {
+        return [
+          TableEvent(
+              title: docList[i]["subject"].toString(),
+              start: TableEventTime(
+                  hour: docList[i]["startHour"],
+                  minute: docList[i]["startMin"]),
+              end: TableEventTime(
+                  hour: docList[i]["endHour"], minute: docList[i]["endMin"]),
+              decoration: BoxDecoration(color: Colors.blue),
+              textStyle: TextStyle(fontSize: 12),
+              padding: EdgeInsets.all(3.0))
+        ];
+      }
+    }
   }
 
   List<LaneEvents> _buildLaneEvents() {
     return [
       LaneEvents(
           lane: Lane(
-              name: widget.dayList[0].toString(),
+              name: '월',
               width: 60,
               textStyle: TextStyle(color: Colors.grey)),
-          events: _buildTableEvent()),
+          events: _buildTableEvent1()),
       LaneEvents(
           lane: Lane(
-              name: widget.dayList[1].toString(),
+              name: '화',
               width: 60,
               textStyle: TextStyle(color: Colors.grey)),
-          events: _buildTableEvent()),
+          events: _buildTableEvent2()),
       LaneEvents(
           lane: Lane(
-              name: widget.dayList[2].toString(),
+              name: '수',
               width: 60,
               textStyle: TextStyle(color: Colors.grey)),
-          events: _buildTableEvent()),
+          events: _buildTableEvent3()),
       LaneEvents(
           lane: Lane(
-              name: widget.dayList[3].toString(),
+              name: '목',
               width: 60,
               textStyle: TextStyle(color: Colors.grey)),
-          events: _buildTableEvent()),
+          events: _buildTableEvent4()),
       LaneEvents(
           lane: Lane(
-              name: widget.dayList[4].toString(),
+              name: '금',
               width: 60,
               textStyle: TextStyle(color: Colors.grey)),
-          events: _buildTableEvent()),
+          events: _buildTableEvent5()),
     ];
   }
 }
